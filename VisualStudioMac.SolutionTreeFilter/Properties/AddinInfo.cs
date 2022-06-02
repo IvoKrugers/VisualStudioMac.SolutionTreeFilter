2ec467c03187c20ac3c3472b43aef1517d38e678
Ivo Krugers <i.krugers@xablu.com>
2022-06-02 13:31:24 +0200

    Update addin url

--- VisualStudioMac.SolutionTreeFilter/Properties/AddinInfo.cs
+++ VisualStudioMac.SolutionTreeFilter/Properties/AddinInfo.cs
@@ -12,7 +12,7 @@ using VisualStudioMac.SolutionTreeFilter;
 [assembly: AddinName("SolutionTree Filter")]
 [assembly: AddinDescription("This extension adds a Pad which allows you to filter the solution tree by multiple search terms.\n\nby Ivo Krugers")]
 [assembly: AddinAuthor("Ivo Krugers")]
-[assembly: AddinUrl("https://github.com/IvoKrugers")]
+[assembly: AddinUrl("https://github.com/IvoKrugers/VisualStudioMac.SolutionTreeFilter")]
 
 [assembly: AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
 [assembly: AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
