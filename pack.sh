da7a3b65076e504c4f09337f821b1d7feca12dbb
Ivo Krugers <i.krugers@xablu.com>
2022-06-14 23:50:49 +0200

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 clear
 
+# Get and goto folder of this script's location
 SCRIPTFILE=$0
-
-#Get the absolute path to the containing folder
 PROJECTFOLDER=${SCRIPTFILE%/*}
-
 cd ${PROJECTFOLDER}
+PROJECTFOLDER=$(pwd)
 
-pwd
-
+# Clean
 rm *.mpack
+
+# Pack
 mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ./VisualStudioMac.SolutionTreeFilter/bin/VisualStudioMac.SolutionTreeFilter.dll
+
+# Uninstall
+/Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool setup uninstall VisualStudioMac.SolutionTreeFilter -y
+
+# Install
+for filename in *.mpack;
+do
+  echo "$filename"
+  /Applications/Visual\ Studio\ \(Preview\).app/Contents/MacOS/vstool setup install "$PROJECTFOLDER/$filename" -y
+done
